36fe8a710ac5bae8df4fe46eb11cd0600a6b7914
KogtaSaloni <saloni13155@iiitd.ac.in>
2014-12-26 17:56:39 +0530

    Index page done with a flaw though(to be corrected)

--- app/models/on_time.rb
+++ app/models/on_time.rb
@@ -1,2 +1,20 @@
 class OnTime < ActiveRecord::Base
+belongs_to :user
+
+  # ensure that a user_id is present
+  validates :user_id, presence: true
+
+  # ensure that title is present and at least 10 chars long
+  validates :task, length: { minimum: 10 }, presence: true
+
+  # ensure the url is present, and respects the URL format for http/https
+  #validates :url, format: {with: Regexp.new(URI::regexp(%w(http https)))}, presence: true
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_onTime
+      unless @OnTime = current_user.on_times.where(id: params[:id]).first
+        flash[:alert] = 'OnTime not found.'
+        redirect_to root_url
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'site/index'
+
   devise_for :users
   resources :on_times
 
@@ -56,4 +58,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'site#index'
 end
